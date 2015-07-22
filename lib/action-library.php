76a62ac6a906cc2856a1b7dc95d5a54941452869
tech-prodigy3 <ajay.singh@tech-prodigy.org>
2015-07-22 18:10:04 +0530

    change made at 06:08
    issues fixed
    =======
    1. Remove mandatory mark(*) from Custom CSS field.

--- lib/action-library.php
+++ lib/action-library.php
@@ -76,7 +76,6 @@ else
 						$update_logo_settings["max_height"] = esc_attr($form_data["ux_txt_width"]);
 						$update_logo_settings["max_width"] = esc_attr($form_data["ux_txt_height"]);
 						$update_logo_settings["custom_css_logo"] = esc_attr($form_data["ux_txtarea_css_logo"]);
-						
 
 						foreach($update_logo_settings as $keys => $value)
 						{
@@ -95,15 +94,15 @@ else
 						parse_str($_REQUEST["data"],$form_data);
 						$update_heading_settings = array();
 						$where = array();
-						
-						$update_heading_settings["heading_settings"] = esc_attr($form_data["ux_ddl_heading_settings"]);
-						$update_heading_settings["heading_text"] = esc_attr($form_data["ux_heading_content"]);
-						$update_heading_settings["font_style_heading"] = esc_attr(implode(",",$form_data["ux_ddl_font_style_heading"]));
-						$update_heading_settings["font_family_heading"] = esc_attr($form_data["ux_ddl_font_family_heading"]);
-						$update_heading_settings["heading_position"] = esc_attr($form_data["ux_ddl_heading_position"]);
+
+						$update_heading_settings["heading_settings"] = esc_attr($form_data["ux_ddl_heading_settings_subscription"]);
+						$update_heading_settings["heading_text"] = esc_attr($form_data["ux_heading_content_subscription"]);
+						$update_heading_settings["font_style_heading"] = esc_attr(implode(",",$form_data["ux_ddl_font_style_heading_subscription"]));
+						$update_heading_settings["font_family_heading"] = esc_attr($form_data["ux_ddl_font_family_heading_subscription"]);
+						$update_heading_settings["heading_position"] = esc_attr($form_data["ux_ddl_heading_positions"]);
 						$update_heading_settings["margin_heading"] = esc_attr(implode(",",$form_data["ux_txt_heading_margin_text"]));
 						$update_heading_settings["padding_heading"] = esc_attr(implode(",",$form_data["ux_txt_heading_padding_text"]));
-						
+
 						foreach($update_heading_settings as $keys => $value)
 						{
 							$update_heading_value = array();
@@ -114,14 +113,14 @@ else
 					}
 					die();
 				break;
-				
+
 				case "description_settings_module" :
 					if(wp_verify_nonce($_REQUEST["_wp_nonce"], "coming_soon_master_description_settings"))
 					{
 						parse_str($_REQUEST["data"],$form_data);
 						$update_description_settings = array();
 						$where = array();
-				
+
 						$update_description_settings["description_settings"] = esc_attr($form_data["ux_ddl_description_settings"]);
 						$update_description_settings["description_text"] = esc_attr($form_data["ux_description_content"]);
 						$update_description_settings["font_style_description"] = esc_attr(implode(",",$form_data["ux_ddl_font_style_des"]));
@@ -129,7 +128,7 @@ else
 						$update_description_settings["description_position"] = esc_attr($form_data["ux_ddl_description_position"]);
 						$update_description_settings["margin_description"] = esc_attr(implode(",",$form_data["ux_txt_des_margin_text"]));
 						$update_description_settings["padding_description"] = esc_attr(implode(",",$form_data["ux_txt_des_padding_text"]));
-				
+
 						foreach($update_description_settings as $keys => $value)
 						{
 							$update_description_value = array();
@@ -140,14 +139,14 @@ else
 					}
 					die();
 				break;
-				
+
 				case "footer_settings_module" :
 					if(wp_verify_nonce($_REQUEST["_wp_nonce"], "coming_soon_master_footer_settings"))
 					{
 						parse_str($_REQUEST["data"],$form_data);
 						$update_footer_settings = array();
 						$where = array();
-				
+
 						$update_footer_settings["footer_settings"] = esc_attr($form_data["ux_ddl_footer_settings"]);
 						$update_footer_settings["footer_text"] = esc_attr($form_data["ux_footer_content"]);
 						$update_footer_settings["font_style_footer"] = esc_attr(implode(",",$form_data["ux_ddl_font_style_ftr"]));
@@ -155,7 +154,7 @@ else
 						$update_footer_settings["footer_position"] = esc_attr($form_data["ux_ddl_footer_position"]);
 						$update_footer_settings["margin_footer"] = esc_attr(implode(",",$form_data["ux_txt_footer_margin_text"]));
 						$update_footer_settings["padding_footer"] = esc_attr(implode(",",$form_data["ux_txt_footer_padding_text"]));
-				
+
 						foreach($update_footer_settings as $keys => $value)
 						{
 							$update_footer_settings = array();
@@ -166,17 +165,17 @@ else
 					}
 					die();
 				break;
-				
+
 				case "favicon_settings_module" :
 					if(wp_verify_nonce($_REQUEST["_wp_nonce"], "coming_soon_master_favicon_settings"))
 					{
 						parse_str($_REQUEST["data"],$form_data);
 						$update_favicon_settings = array();
 						$where = array();
-				
+
 						$update_favicon_settings["favicon_settings"] = esc_attr($form_data["ux_ddl_favicon_settings"]);
 						$update_favicon_settings["upload_favicon"] = esc_attr($form_data["ux_txt_upload"]);
-						
+
 						foreach($update_favicon_settings as $keys => $value)
 						{
 							$update_favicon_settings = array();
